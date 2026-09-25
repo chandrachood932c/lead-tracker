5d733b582a8cc0d7b34d5f42e58df2968fb80220
Chandrachood <chandrachood.chatarkar@namastecredit.com>
2026-09-25 14:41:36 +0530

    minor changes for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -7,8 +7,12 @@ const leadRoutes = require('./routes/leadRoutes');
 
 const app = express();
 
-app.use(cors())
-app.use(express.json());
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL || "http://localhost:5173"
+  })
+);
+app.use(express.json({ limit: "10kb" }));
 
 app.get("/api/health", (req,res)=>{
     res.json({
@@ -21,12 +25,16 @@ app.use("/api/leads", leadRoutes);
 
 const PORT = process.env.PORT || 5000;
 
-const startServer = async () => {
+if (process.env.NODE_ENV !== "production") {
+  const startServer = async () => {
     await connectDB();
 
     app.listen(PORT, () => {
-        console.log(`Server is running on port ${PORT}`);
+      console.log(`Server running on http://localhost:${PORT}`);
     });
-};
+  };
 
-startServer();
+  startServer();
+}
+
+module.exports = app;
